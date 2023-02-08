0074fc2a150f66f0aa94b30723877e6d5c3f9568
Your Name <you@example.com>
2023-02-08 20:49:23 +0100

    main modificado

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import exercici1_B as ex1B
 import exercici2_B as ex2B
 import exercici3_B as ex3B
+import exercici1_A as ex1A
+import exercici2_A as ex2A
+import exercici3_A as ex3A
 
+print(ex1A.numeroGran())
+print(ex2A.nombres5())
+print(ex3A.aleatorio())
 print(ex1B.exercici1B())
 print(ex2B.exercici2B())
 print(ex3B.exercici3B())
